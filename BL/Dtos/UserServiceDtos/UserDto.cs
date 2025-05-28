a80f3c7db4395a64e76b0f2f2fdf2ef5b19e3ac2
mohammed <mohasobhy6@gmail.com>
2025-05-28 17:10:25 +0300

    rework DataAnnotations

--- BL/Dtos/UserServiceDtos/UserDto.cs
+++ BL/Dtos/UserServiceDtos/UserDto.cs
@@ -1,4 +1,6 @@
-﻿using BL.Dtos.Base;
+﻿using AppResources.Messages;
+using BL.Dtos.Base;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace BL.Dtos.UserServiceDtos
@@ -6,20 +8,18 @@ namespace BL.Dtos.UserServiceDtos
 	public class UserDto : BaseDto
 	{
 
-		//[Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Messages), AllowEmptyStrings = false)]
-		//[StringLength(100, MinimumLength = 5, ErrorMessage = "NameLenght", ErrorMessageResourceType = typeof(Messages))]
+		[Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(message), AllowEmptyStrings = false)]
 		public string? UserName { get; set; }
 
-		//[Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Messages), AllowEmptyStrings = false)]
-		//[EmailAddress]
-		public string? Email { get; set; }
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(message), AllowEmptyStrings = false)]
 
+        public string? Email { get; set; }
+         
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(message), AllowEmptyStrings = false)]
+        public string? Password { get; set; }
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(message), AllowEmptyStrings = false)]
 
-		//[Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Messages), AllowEmptyStrings = false)]
-		public string? Password { get; set; }
-
-		//[Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Messages), AllowEmptyStrings = false)]
-		public string? ConfirmPassword { get; set; }
+        public string? ConfirmPassword { get; set; }
 		public string? ReturnUrl { get; set; }
 	}
 }
