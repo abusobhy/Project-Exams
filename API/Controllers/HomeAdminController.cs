735c8e168fb560ad48e89705a1e0363dc329eee7
mohammed <mohasobhy6@gmail.com>
2025-05-30 15:14:23 +0300

    updata Home and add HomeAdmin

---
+++ API/Controllers/HomeAdminController.cs
@@ -0,0 +1,31 @@
+﻿using BL.Contracts;
+using BL.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace API.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class HomeAdminController : ControllerBase
+	{
+		private readonly ILogger<HomeAdminController> _logger;
+		private readonly IVwSEQuestion _vwSEQuestion;
+
+		public HomeAdminController(ILogger<HomeAdminController> logger, IVwSEQuestion vwSEQuestion)
+		{
+			_logger = logger;
+			_vwSEQuestion = vwSEQuestion;
+		}
+
+		// GET: api/<HomeController>
+		[HttpGet]
+		public IEnumerable<VwSEQuestionDto> GetSubjectsExamsQuestions()
+		{
+			return _vwSEQuestion.GetAll().ToList();
+		}
+
+		
+	}
+}
--- API/Controllers/HomeController.cs
+++ API/Controllers/HomeController.cs
@@ -3,59 +3,32 @@ using BL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace API.Controllers
 {
-	[Route("api/[controller]")]
+	[Route("api/[controller]/[action]")]
 	[ApiController]
 	public class HomeController : ControllerBase
 	{
 		private readonly ILogger<HomeController> _logger;
-		private readonly ISubjectsTaught _subjects;
-		//private readonly IQuestion _question;
-		private readonly IExam _exams;
+		private readonly IVwSubjectTaughtExam _vwSubjectTaughtExams;
 
-		public HomeController(ISubjectsTaught subjects, ILogger<HomeController> logger, IExam exams)
+		public HomeController(ILogger<HomeController> logger, IVwSubjectTaughtExam vwSubjectTaughtExams)
 		{
-			_subjects = subjects;
 			_logger = logger;
-			_exams = exams;
-			//_question = question;
+			_vwSubjectTaughtExams = vwSubjectTaughtExams;
 		}
 
 
 
 		// GET: api/<HomeController>
 		[HttpGet]
-		public IEnumerable<SubjectsTaughtDto> Get()
+		public IEnumerable<VwSubjectTaughtExamDto> GetSubjectsExams()
 		{
-			return _subjects.GetAll().ToList();
+			return _vwSubjectTaughtExams.GetAll().ToList();
 		}
+	
 
-		// GET api/<HomeController>/5
-		[HttpGet("{id}")]
-		public string Get(int id)
-		{
-			return "value";
-		}
-
-		// POST api/<HomeController>
-		[HttpPost]
-		public void Post([FromBody] string value)
-		{
-		}
-
-		// PUT api/<HomeController>/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] string value)
-		{
-		}
-
-		// DELETE api/<HomeController>/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
-		{
-		}
+		
 	}
 }
